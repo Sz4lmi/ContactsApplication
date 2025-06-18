38349c149f0d4045931931052e6a7663ead75193
Sz4lmi <aron.szalmas@gmail.com>
2025-06-19 01:16:00 +0200

    fixed dummy users

--- src/main/resources/db/migration/V6__add_dummy_users.sql
+++ src/main/resources/db/migration/V6__add_dummy_users.sql
@@ -1,3 +1,5 @@
 INSERT INTO "user" (username, password, role) VALUES
-    ('test1', '$2a$10$7ZMYNdtXBGRN14ZLGxdxuuLUFmXuWlGeEon.5ZTTRoC1C6x0cq4Zi', 'ROLE_USER'),
-    ('test2', '$2a$10$llX9AkdvkVHyyms0yWg9YuXoHj0VSFEWJN7zyYzyjTuNWqdQZ63ES', 'ROLE_USER');
+    ('tesztelek', '$2a$10$7Ih6jBiGvI9PvnFimx1mduG.NdSGWnstkI5AVvS3Ite6lZE.F4giC', 'ROLE_USER'),
+    ('tesztbela', '$2a$10$SChTuYNyeWCqJiBgr9/dG.3nUURGHh6UMIidw/WA964clW0rXCv2q', 'ROLE_USER'),
+    ('tesztandras', '$2a$10$BNMt.MEZJTE5i0DXh/mfQOOSLJvUhwgxuHOXTKiJQgAaxzF9fNili', 'ROLE_USER'),
+    ('teszterno', '$2a$10$fG0TUuF8ebsysTwYo3X.fOddT4Rh9bg3NirCRdvFpeKn2oTxp33ny', 'ROLE_USER');
